0feb196d93e24ae3770f2b3a67c68ecaee07cf41
Bhavya Shah <bhavyashah8443@gmail.com>
2025-03-20 11:50:01 +0530

    feat: Added clone.c for test and start implementation for abthread_create()
    The clone.c is example listed to let understand how clone actually works.

---
+++ testCodes/clone.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+#define _SCHED_H 1
+#define __USE_GNU 1
+#include <bits/sched.h>
+
+#define STACK_SIZE 4096
+
+int func(void *arg) {
+    int count = *((int *)arg);
+    printf("Inside func.\n");
+    sleep(10*count);
+    printf("Terminating func...\n");
+
+    return 1;
+}
+
+int main() {
+    printf("This process pid: %u\n", getpid());
+    char status_file[] = "/proc/self/status";
+    char *child_stack1 = (char *)malloc(STACK_SIZE);
+    char *child_stack2 = (char *)malloc(STACK_SIZE);
+    char *tmp = child_stack1;
+    int thread_pid1, thread_pid2;
+    FILE *fp = fopen(status_file, "rb");
+
+    printf("Looking into %s...\n", status_file);
+
+    while(1) {
+        char ch = fgetc(fp);
+        if(feof(fp)) break;
+        printf("%c", ch);
+    }
+    printf("Creating new thread...\n");
+    int count1 = 1;
+    thread_pid1 = clone(&func, child_stack1+STACK_SIZE, SIGCHLD |CLONE_SIGHAND|CLONE_FS|CLONE_VM|CLONE_FILES | CLONE_THREAD, &count1, NULL);
+    int count2 = 2;
+    thread_pid2 = clone(&func, child_stack2+STACK_SIZE, SIGCHLD |CLONE_SIGHAND|CLONE_FS|CLONE_VM|CLONE_FILES | CLONE_THREAD, &count2, NULL);
+    printf("Done! Thread pid: %d\n", thread_pid1);
+    printf("Done! Thread pid: %d\n", thread_pid2);
+
+    FILE *fp = fopen(status_file, "rb");
+
+    printf("Looking into %s...\n", status_file);
+
+    while(1) {
+        char ch = fgetc(fp);
+        if(feof(fp)) break;
+        printf("%c", ch);
+    }
+
+    fclose(fp);
+    int wstatus;
+    waitpid(thread_pid1, &wstatus, __WCLONE);
+    printf("Started, %d", wstatus);
+    getchar();
+    printf("%d ", (child_stack1-tmp));
+    return 0;
+}
